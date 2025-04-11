8cc03f4e9cb72f51b65f089005589df9eb365eab
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-04-11 14:00:21 +0700

    add

--- JavaScrip/Phan-7/Thuc-hanh-2/Thuc-hanh-2.js
+++ JavaScrip/Phan-7/Thuc-hanh-2/Thuc-hanh-2.js
@@ -1,7 +1,15 @@
- inputWidth = prompt ("Enter the width");
+/* inputWidth = prompt ("Enter the width");
  inputHeight = prompt ("Enter the height");
 
  let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height ;
-document.write("The area is: " + area);
+document.write("The area is: " + area);*/
+
+function getHcn() {
+ let width = +document.getElementById("width").value;
+ let height = +document.getElementById("height").value;
+ let area = width * height;
+document.write(area);
+}
+
