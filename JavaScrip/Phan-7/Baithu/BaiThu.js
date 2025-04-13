6aec89d1da66910178dbafed6a7ce8f459d20b22
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-04-13 08:07:00 +0700

    Add

---
+++ JavaScrip/Phan-7/Baithu/BaiThu.js
@@ -0,0 +1,10 @@
+let kg = prompt("Can nang: ");
+if  (kg < 50) {
+    alert("Size S")
+} else if (kg < 65 ) {
+    alert("size M")
+} else if (kg < 80) {
+    alert("size L")
+} else {
+    alert("size XL")
+}
